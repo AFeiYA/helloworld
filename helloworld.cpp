076e7fee111f916e80031803beecb4bfc2c80ce7
AFeiYA <386665678@qq.com>
2024-07-23 11:40:33 +0800

    添加项目文件。

---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+﻿// helloworld.cpp: 定义应用程序的入口点。
+//
+
+#include "helloworld.h"
+#include <fmt/core.h>
+
+using namespace std;
+
+int main()
+{
+	fmt::print("Hello Cmake and vcpkg!\n");
+	return 0;
+}
